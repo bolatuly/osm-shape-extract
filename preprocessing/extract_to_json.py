e265d719132920d3c07d39ec54eab86516105545
bolatuly <b.a.bolatuly@gmail.com>
2017-10-08 18:04:28 +0900

    write to geojson and visualize

---
+++ preprocessing/extract_to_json.py
@@ -0,0 +1,36 @@
+import osmium
+import shapely.wkb as wkblib
+from preprocessing.algorithm.polygon import Polygon
+from geojson import FeatureCollection
+import pandas as pd
+import geojson
+
+wkbfab = osmium.geom.WKBFactory()
+
+
+class BuildingHandler(osmium.SimpleHandler):
+    def __init__(self):
+        osmium.SimpleHandler.__init__(self)
+        self.buildings = []
+        self.exception_counter = 0
+
+    def area(self, a):
+        if 'building' in a.tags:
+            if a.tags['building'] == 'house':
+                try:
+                    wkb = wkbfab.create_multipolygon(a)
+                    poly = wkblib.loads(wkb, hex=True)
+                    g2= geojson.Feature(id=a.id, geometry=poly, properties={})
+                    self.buildings.append(g2)
+                except:
+                    self.exception_counter += 1
+
+
+if __name__ == '__main__':
+    h = BuildingHandler()
+    h.apply_file("../data/south-korea-latest.osm.pbf")
+    feature_collection = FeatureCollection(h.buildings)
+    print("Objects with broken geometry: " + str(h.exception_counter))
+    import json
+    with open('../data/result/south_korea_houses_geojson.json', 'w') as f:
+        json.dump(feature_collection, f, ensure_ascii=False)
